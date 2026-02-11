6255a2d6ef74372430ec3cf0b1135e14bba71463
golem <78136824+animegolem@users.noreply.github.com>
2026-02-10 18:47:00 -0600

    Refactor configuration options in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -55,8 +55,8 @@ const  preference_bounds_name = "Bounds",  // Preference bounds name
        default_tilt = -1,                            // Default Custom DF tilt value
        default_ear = -0,                                // Default Custom DF ear gain value
        default_treble = -2,                             // Default Custom DF treble gain value
-       tiltableTargets = ["Golem HRTF", "waitgodwhat", "pleasestopgodwhy", "ARI, HUTUBS, RIEC, SONICOM AVG","ISO 11904"],                 // Targets that are allowed to be tilted
-       compTargets = ["Golem HRTF", "waitgodwhat", "pleasestopgodwhy", "ARI, HUTUBS, RIEC, SONICOM AVG","ISO 11904"],                     // Targets that are allowed to be used for compensation
+       tiltableTargets = ["Golem HRTF", "Left Only", "Right Only", "ARI, HUTUBS, RIEC, SONICOM AVG","ISO 11904"],                 // Targets that are allowed to be tilted
+       compTargets = ["Golem HRTF", "Left Only", "Right Only", "ARI, HUTUBS, RIEC, SONICOM AVG","ISO 11904"],                     // Targets that are allowed to be used for compensation
        allowCreatorSupport = false;                     // Allow the creator to have a button top right to support them
        allowLanguageSelector = false;                   // Add Language Selector on the top right of the page. If it's false, l10n feature will be disabled.
        availableLanguages = ["en", "ko"];              // List of available language codes. When you are adding a new language, make sure to use ISO 639-1 Language Codes for auto-detection.
@@ -269,4 +269,5 @@ let tutorialDefinitions = [
 ]
 
 let whichTutorialDefinitionsToUse = tutorialDefinitions;
+
 
